b801c3587aef5e5803b06cc4dfaa60e750a3e9fa
shaikhaqibpasha <106161025+shaikhaqibpasha@users.noreply.github.com>
2022-11-30 03:15:35 +0530

    first commit

---
+++ Occurance.js
@@ -0,0 +1,117 @@
+////**** "SAME" : COUNTANTY ****/
+
+// const arr=[2,5,1,2,5,2,5,7,2,5]
+
+// const obj={};
+
+// arr.forEach((item)=>
+//    (obj[item]= obj[item] ? obj[item] + 1  : 1 ));
+
+//    console.log(obj);
+
+// const string ="I AM FROM NANDED";
+
+// let arr1=str.split("").reverse(" ").join("").split(" ").reverse(" ").join(" ")
+// console.log(arr1)
+
+// console.log(arr1)
+
+// const mynum="7756903575"
+// let newnum=mynum.split("")
+// let result=newnum.reduce((acc,cur)=>Number(acc)+Number(cur))
+// console.log(result)
+
+// const string ="I AM FROM NANDED"
+
+// let newarr=string.split(" ")
+// let result=newarr.reduce((acc,cur)=>acc.length>cur.length?acc:cur)
+// console.log(result)
+
+// var x="A"
+// var y="10"
+
+// // var temp=x
+// // x=y
+// // y=temp
+// [x]=[y]
+// [y]=[x]
+// console.log("x=>",x,"y=>",y)
+// console.log(x,y)
+
+// const mynum  = "7756903575"
+// let newnum=mynum.split("")
+// let sum  =0
+// for (let i=0;i<newnum.length;i++){
+//   sum=sum+Number(newnum[i])
+
+// }
+// console.log(sum)
+
+
+// function reversestr(str) {
+
+//     return str.split("").reverse().join("")
+
+
+// }
+// console.log(reversestr("PDAC"))
+// console.log(reversestr("Pune"))
+// console.log(reversestr("136"))
+// console.log(reversestr("Sam"))
+// console.log(reversestr("John"))
+
+// function modulesnum(num){
+// if(num%2===0){
+//     console.log("Fizz")
+// }
+// else if(num%3===0){
+//     console.log("Buzz")
+// }
+// else if(num%5===0){
+//     console.log("FizzBuzz")
+// }
+
+// }
+// modulesnum(45)
+
+// for (let i=0;i<10;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     },1000);
+
+// }
+
+// if(100<200){
+//     var a="One"
+//     let b="Two"
+//     const c="Three"
+//     // console.log(b,c)
+// }
+// console.log(a,b,c)
+
+// Index same in line and change line ******
+
+const array1=["A","B","C"]
+const array2=["X","Y","Z"]
+array1.splice(2,0,...array2)
+console.log(array1)
+
+//  O/P=["A","B","X","Y","Z","C" ]
+
+
+
+// Reverse method for using forloop *******
+
+// const array = ["aqib", "pasha", "tamsa"];
+// array.split("").reverse("").join("").split("").reverse("").join("")
+// console.log(array)
+
+
+// length=array.length
+// newarray=[]
+// for(let i= length-1;i>-1;i--){
+//     newarray.push(array[i])
+// }
+// // console.log(newarray)
+
+// newarray=( [ 'tamsa', 'pasha', 'aqib' ])
